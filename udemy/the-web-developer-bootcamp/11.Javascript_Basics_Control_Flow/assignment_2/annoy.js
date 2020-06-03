4eff15ffef7cd7827f31143efce54c02ae6824cf
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-03 14:54:56 +0530

    assignment 2

--- udemy/the-web-developer-bootcamp/11.Javascript_Basics_Control_Flow/assignment_2/annoy.js
+++ udemy/the-web-developer-bootcamp/11.Javascript_Basics_Control_Flow/assignment_2/annoy.js
@@ -1,7 +1,9 @@
-while (true) {
+var inp = prompt("Are we there yet");
+while (inp.indexOf("yes") === -1 && inp.indexOf("yeah")) {
     var inp = prompt("Are we there yet");
-    if (inp == "yes" || inp == "yeah") {
-        alert("Yay, we finally made it...!!");
-        break;
-    }
+
 }
+// while (inp !== "yes" && inp !== "yeah") {
+//     var inp = prompt("Are we there yet");
+// }
+alert("Yay, we finally made it...!!");
