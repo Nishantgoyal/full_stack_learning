a9058d42b1bc6d255de99bcd4f7405edf218bdb9
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-09 14:06:17 +0530

    done assignment 2

--- udemy/the-web-developer-bootcamp/16.Advanced_DOM_Manipulation/assignment_2/script.js
+++ udemy/the-web-developer-bootcamp/16.Advanced_DOM_Manipulation/assignment_2/script.js
@@ -1,24 +1,42 @@
 function increase_score() {
+    if (winner) { return; }
     player_id = this.getAttribute("id")
     var player_counter = document.querySelector("#" + player_id + "_Score");
-    player_counter.textContent++;
+    current_score = Number(player_counter.textContent);
+    if (current_score < max_score) {
+        current_score++;
+        player_counter.textContent = current_score;
+        if (current_score === max_score) {
+            player_counter.style.color = "green";
+            winner = true;
+        }
+    }
 }
 
 function reset_score() {
-    console.log("Resetting Score");
     scores = document.querySelectorAll(".scores");
     for (var i = 0; i < scores.length; i++) {
         scores[i].textContent = 0;
+        scores[i].style.color = "black";
     }
+    winner = false;
+}
+
+function set_score_selector() {
+    var score_selector = document.querySelector("#score_selector");
+    score_selector.selectedIndex = 0;
 }
 
 function set_max_score(event) {
     var max_counter = document.querySelector("#max_counter");
-    // console.log(event);
     max_counter.textContent = event.target.value;
+    max_score = Number(event.target.value);
+    reset_score()
 }
 
 function main() {
+
+
     // Score should increase on clicking the button
     var players = document.querySelectorAll(".players");
     for (var i = 0; i < players.length; i++) {
@@ -28,10 +46,13 @@ function main() {
     // Reset
     var reset_button = document.querySelector("#reset");
     reset_button.addEventListener("click", reset_score);
+    // reset_button.addEventListener("click", set_score_selector);
 
     // Setting Max Score
     var score_selector = document.querySelector("#score_selector");
     score_selector.addEventListener("change", set_max_score);
 }
 
+var max_score = 5;
+var winner = false;
 main()
