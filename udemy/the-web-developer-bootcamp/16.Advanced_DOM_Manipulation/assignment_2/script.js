dfb485ddd29fb811c37de0651f8fd0cf305fcbf0
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-09 12:57:33 +0530

    done reset

--- udemy/the-web-developer-bootcamp/16.Advanced_DOM_Manipulation/assignment_2/script.js
+++ udemy/the-web-developer-bootcamp/16.Advanced_DOM_Manipulation/assignment_2/script.js
@@ -1,16 +1,27 @@
-// Required events
-// 1. Score should increase on clicking the button
 function increase_score() {
     player_id = this.getAttribute("id")
     var player_counter = document.querySelector("#" + player_id + "_Score");
     player_counter.textContent++;
 }
 
+function reset_score() {
+    console.log("Resetting Score");
+    scores = document.querySelectorAll(".scores");
+    for (var i = 0; i < scores.length; i++) {
+        scores[i].textContent = 0;
+    }
+}
+
 function main() {
+    // Score should increase on clicking the button
     var players = document.querySelectorAll(".players");
     for (var i = 0; i < players.length; i++) {
         players[i].addEventListener("click", increase_score);
     }
+
+    // Reset
+    var reset_button = document.querySelector("#reset");
+    reset_button.addEventListener("click", reset_score);
 }
 
 main()
