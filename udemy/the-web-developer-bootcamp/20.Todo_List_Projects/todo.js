cc9472b2a59a94e7c6bc4d7c7a909fd1fb2d1750
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-18 14:06:18 +0530

    added delegated click event on li

--- udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
+++ udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
@@ -1,10 +1,11 @@
 $("#todo_input").on("keypress", function(event) {
     if (event.which === 13) {
         var value = $(this).val();
-        var li = document.createElement("li");
-        li.textContent = value;
-        $("ul").append(li)
-        console.log(value);
+        $("ul").append("<li>" + value + "</li>");
         $(this).val("");
     }
+});
+
+$("ul").on("click", "li", function() {
+    console.log($("li"));
 });
