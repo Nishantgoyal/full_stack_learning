518efb38e69e28de8f9eca815031f5e3403ae602
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-18 14:36:35 +0530

    add hover effect for toggling delete button

--- udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
+++ udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
@@ -1,11 +1,20 @@
 $("#todo_input").on("keypress", function(event) {
     if (event.which === 13) {
-        var value = $(this).val();
-        $("ul").append("<li>" + value + "</li>");
+        var row = "<tr><td><div class='delete'>x</div>" + $(this).val() + "</td></tr>";
+        $("#TODO_list").append(row);
         $(this).val("");
     }
 });
 
-$("ul").on("click", "li", function() {
+$("table").on("click", "tr", function() {
+
     console.log($("li"));
+});
+
+
+$("#TODO_list").on("mouseover", "tr", function() {
+    $(this).find(".delete").css("display", "inline-block");
+});
+$("#TODO_list").on("mouseout", "tr", function() {
+    $(this).find(".delete").css("display", "none");
 });
