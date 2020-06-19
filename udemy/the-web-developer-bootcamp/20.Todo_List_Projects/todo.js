2ba7446d3ec78f2f1a2247ef5320b4967a323b73
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-19 14:44:33 +0530

    done todo list project

--- udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
+++ udemy/the-web-developer-bootcamp/20.Todo_List_Projects/todo.js
@@ -1,22 +1,54 @@
 $("#todo_input").on("keypress", function(event) {
     if (event.which === 13) {
-        var row = "<tr><td><div class='delete'>x</div>" + $(this).val() + "</td></tr>";
+        var row = get_row($(this).val())
         $("#TODO_list").append(row);
         $(this).val("");
     }
 });
+
+function get_delete_icon() {
+    var del = document.createElement("div");
+    del.classList.add("hide");
+    del.classList.add("delete");
+    del.innerHTML = '<div> <i id="hide_search" class="fa fa-trash"></i></div>'
+    return del;
+}
+
+function get_row(value) {
+    var ele = document.createElement("tr");
+    var td = document.createElement("td");
+
+    var del = get_delete_icon();
+    td.appendChild(del);
+
+    var item = document.createElement("div");
+    item.classList.add("item");
+    item.textContent = value;
+
+    td.appendChild(item);
+    ele.appendChild(td);
+    return ele;
+}
 
 $("#TODO_list").on("click", "tr", function() {
     $(this).toggleClass("strike");
 });
 
 
-$("#TODO_list").on("mouseover", "tr", function() {
-    $(this).find(".delete").css("display", "inline-block");
+$("#TODO_list").on("mouseenter", "tr", function() {
+    $(this).find(".delete").removeClass("hide");
+    $(this).find(".delete").animate({
+        width: "35px"
+    }, 400);
 });
 
-$("#TODO_list").on("mouseout", "tr", function() {
-    $(this).find(".delete").css("display", "none");
+$("#TODO_list").on("mouseleave", "tr", function() {
+    var ele = $(this).find(".delete");
+    $(this).find(".delete").animate({
+        width: 0
+    }, 200, function() {
+        ele.addClass("hide");
+    });
 });
 
 
@@ -25,5 +57,6 @@ $("#TODO_list").on("click", ".delete", function() {
 });
 
 $("#hide_search").on("click", function() {
-    $("#todo_input").toggleClass("hide");
-})
+
+    $("#todo_input").fadeToggle();
+});
