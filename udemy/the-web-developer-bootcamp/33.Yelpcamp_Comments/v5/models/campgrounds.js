915594ca5e39d7cd1e0722eb8b15975ff4bc5581
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-17 15:31:54 +0530

    styling show page

--- udemy/the-web-developer-bootcamp/33.Yelpcamp_Comments/v5/models/campgrounds.js
+++ udemy/the-web-developer-bootcamp/33.Yelpcamp_Comments/v5/models/campgrounds.js
@@ -4,7 +4,7 @@ module.exports = mongoose.model("Campground",
   new mongoose.Schema({
     name: String,
     image: String,
-    desc: String,
+    description: String,
     comments: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "Comment"
