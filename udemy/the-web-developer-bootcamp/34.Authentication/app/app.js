976d734cfcfce1c1f57f705ce9efa8ac6e35167a
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-17 21:52:39 +0530

    passport setup

--- udemy/the-web-developer-bootcamp/34.Authentication/app/app.js
+++ udemy/the-web-developer-bootcamp/34.Authentication/app/app.js
@@ -1,10 +1,26 @@
-const express   = require("express"),
-      mongoose  = require("mongoose"),
-      app       = express();
+const express               = require("express"),
+      mongoose              = require("mongoose"),
+      passport              = require("passport"),
+      bodyParser            = require("body-parser"),
+      localStrategy         = require("passport-local"),
+      expressSession        = require("express-session"),
+      passportLocalMongoose = require("passport-local-mongoose"),
+      app                   = express();
 
 mongoose.connect("mongodb://db/secret");
+const User = require("./models/user");
+app.use(expressSession({
+  secret: "this is awesome",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
