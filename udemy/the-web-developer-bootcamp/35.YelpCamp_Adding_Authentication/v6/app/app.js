1a6091fe2fb192968488b2ea7f3872404a1d53b4
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-18 19:41:59 +0530

    passport setup

--- udemy/the-web-developer-bootcamp/35.YelpCamp_Adding_Authentication/v6/app/app.js
+++ udemy/the-web-developer-bootcamp/35.YelpCamp_Adding_Authentication/v6/app/app.js
@@ -17,6 +17,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
 
+app.use(require("express-session")({
+  secret: "12345",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res) {
     res.render("landing.ejs");
 });
