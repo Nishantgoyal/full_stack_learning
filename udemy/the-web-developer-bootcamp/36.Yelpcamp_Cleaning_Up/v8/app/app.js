209ff16c4400bb4f0eec25bdb737ee7b58494f14
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-20 12:14:06 +0530

    starting v8

---
+++ udemy/the-web-developer-bootcamp/36.Yelpcamp_Cleaning_Up/v8/app/app.js
@@ -0,0 +1,44 @@
+const express     = require("express"),
+      app         = express(),
+      bodyParser  = require("body-parser"),
+      mongoose    = require("mongoose"),
+      passport    = require("passport"),
+      localStrategy = require("passport-local"),
+      User        = require("./models/users"),
+      seedDB      = require("./seeds");
+
+var campgroundRoutes  = require("./routes/campgrounds"),
+    commentRoutes     = require("./routes/comments"),
+    indexRoutes       = require("./routes/index");
+
+seedDB();
+mongoose.connect("mongodb://db/yelpcamp");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine","ejs");
+
+app.use(require("express-session")({
+  secret: "12345",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(8080, function() {
+    console.log("Yelpcamp app")
+});
---
+++ udemy/the-web-developer-bootcamp/36.Yelpcamp_Cleaning_Up/v8/app/models/comments.js
@@ -0,0 +1,14 @@
+const mongoose    = require("mongoose");
+
+module.exports = mongoose.model("Comment",
+  new mongoose.Schema({
+    text: String,
+    author: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      username: String
+    }
+  })
+);
