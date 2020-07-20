52c79df4f657be4cccabc3b83baeeb643955f70d
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-20 23:01:42 +0530

    edit comment

--- udemy/the-web-developer-bootcamp/37.Yelpcamp_Update_and_Destroy/v10/app/routes/comments.js
+++ udemy/the-web-developer-bootcamp/37.Yelpcamp_Update_and_Destroy/v10/app/routes/comments.js
@@ -35,6 +35,29 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, comment){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("comments/edit", {
+        campground_id : req.params.id, 
+        comment       : comment
+      });
+    }
+  });
+});
+
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
     next();
