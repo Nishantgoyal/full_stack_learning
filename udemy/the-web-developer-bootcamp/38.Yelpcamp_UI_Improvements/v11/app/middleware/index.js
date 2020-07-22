84d6af9004714708b5bd7c7e31293ae626d85266
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-22 17:37:31 +0530

    some helpful messages

--- udemy/the-web-developer-bootcamp/38.Yelpcamp_UI_Improvements/v11/app/middleware/index.js
+++ udemy/the-web-developer-bootcamp/38.Yelpcamp_UI_Improvements/v11/app/middleware/index.js
@@ -7,7 +7,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
   if(req.isAuthenticated()) {
     next();
   } else {
-    req.flash("error", "Please login first");
+    req.flash("error", "Login required..");
     res.redirect("/login");
   }
 }
@@ -16,16 +16,19 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
   if(req.isAuthenticated()) {
     Campground.findById(req.params.id, function(err, campground) {
       if(err) {
+        req.flash("error", "Campground not found");
         res.redirect("back");
       } else {
         if(campground.author.id.equals(req.user.id)) {
           next();
         } else {
+          req.flash("error", "Permission denied");
           res.redirect("back");
         }
       }
     });
   } else {
+    req.flash("error", "Login required..");
     res.redirect("back");
   }
 }
@@ -34,16 +37,19 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
   if(req.isAuthenticated()) {
     Comment.findById(req.params.comment_id, function(err, comment) {
       if(err) {
+        req.flash("error", "Comment not found");
         res.redirect("back");
       } else {
         if(comment.author.id.equals(req.user.id)) {
           next();
         } else {
+          req.flash("error", "Permission denied");
           res.redirect("back");
         }
       }
     });
   } else {
+    req.flash("error", "Login required..");
     res.redirect("back");
   }
 }
